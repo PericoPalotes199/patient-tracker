b99e7ebab3770c2b2cff0c74698e483697edb150
sealocal <local.mat@gmail.com>
2014-11-13 02:36:58 -0800

    use fixed_footer on User#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def conditional_fixed_footer
     if !current_user ||
     (controller_name == 'encounters' && action_name == 'show') ||
-    (controller_name == 'users' && action_name == 'index' && !policy(@users).index?)
+    (controller_name == 'users' && action_name == 'index' && !policy(@users).index?) ||
+    (controller_name == 'users' && action_name == 'show')
       'fixed_footer'
     end
   end
