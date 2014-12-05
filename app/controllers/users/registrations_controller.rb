a43348b833ed79f57b298a0e343de5353ce34caf
sealocal <local.mat@gmail.com>
2014-12-05 01:29:59 -0800

    subscribe customer on sign up and save active_until to user record

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -15,13 +15,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
         customer = Stripe::Customer.create(
           email: sign_up_params[:email],
           description: ENV["STRIPE_CUSTOMER_DESCRIPTION"],
-          card: params[:stripeToken],
-          plan: 'monthly',
-          quantity: 1
+          card: params[:stripeToken]
         )
-
+        subscription = customer.subscriptions.create(plan: 'monthly', quantity: 1)
         save_customer_id(resource, customer.id)
+
+        #TODO: These should probably be ActiveRecord callbacks.
         assign_admin_role(resource)
+        save_active_until(resource, subscription.current_period_end)
 
       rescue Stripe::CardError => e
         flash[:error] = e.message
@@ -31,7 +32,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         #TODO: send an e-mail to application owner
         redirect_to new_user_registration_path
       rescue => e
-        flash[:error] = "unsuccessful. Please contact #{ENV[""]}"
+        flash[:error] = "Unsuccessful. Please contact #{ENV["CONTACT_EMAIL_ADDRESS"]}"
       end
     end
   end
@@ -44,6 +45,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
     def assign_admin_role(user)
       user.role = 'admin'
+      user.save!
+    end
+
+    def save_active_until(user, timestamp)
+      user.active_until = Time.zone.at(timestamp)
       user.save!
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129184056) do
+ActiveRecord::Schema.define(version: 20141204090837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20141129184056) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "customer_id"
+    t.integer  "active_until"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
