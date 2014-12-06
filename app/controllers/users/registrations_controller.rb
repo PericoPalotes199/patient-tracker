51c9ce0eb69bfa152c542e862aa6d639a7aa7ea8
sealocal <local.mat@gmail.com>
2014-12-05 17:31:10 -0800

    add another redirect on error

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -32,6 +32,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
         redirect_to new_user_registration_path
       rescue => e
         flash[:error] = "Unsuccessful. Please contact #{ENV["CONTACT_EMAIL_ADDRESS"]}"
+        redirect_to new_user_registration_path
       end
     end
   end
