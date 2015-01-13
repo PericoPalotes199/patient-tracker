41a05b8fa1747cf17c004a078611220d81635062
sealocal <local.mat@gmail.com>
2015-01-12 19:56:37 -0800

    redirect to sign_in page on sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,4 +37,8 @@ class ApplicationController < ActionController::Base
         super
       end
     end
+
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
 end
