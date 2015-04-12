3107b07fbeb31c0684fdd76b1b0dbb822ae8985e
sealocal <local.mat@gmail.com>
2015-04-12 16:08:00 -0700

    add users controller tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -7,52 +8,68 @@ class UsersController < ApplicationController
     # the two-query implementation needs to remain until
     # residents are assigned a residency on invitation
 
-    if current_user
-      @users = current_user.invitations.to_a
-      # TODO: create a Residency resource and association
-      # get all users of the current_user residency
-      residency_users = User.where(residency: current_user.residency)
+    @users = current_user.invitations.to_a
+    # TODO: create a Residency resource and association
+    # get all users of the current_user residency
+    residency_users = User.where(residency: current_user.residency)
 
-      residency_users.each do |user|
-        # filtering on current_user allows adding an explicit top row for current_user in view
-        @users << user unless @users.include?(user) || user == current_user
-      end
-    else #!current_user
-      @users = User.none
+    residency_users.each do |user|
+      # filtering on current_user allows adding an explicit top row for current_user in view
+      @users << user unless @users.include?(user) || user == current_user
     end
   end
 
   # GET /users/1
   # GET /users/1.json
   def show
-    set_user
+    if !( current_user.admin? && current_user.invitations.include?(@user) || current_user.eql?(@user) )
+      redirect_to current_user
+    end
   end
 
   # GET /users/1/edit
   def edit
+    if !( current_user.admin? && current_user.invitations.include?(@user) || current_user.eql?(@user) )
+      if current_user.admin?
+        redirect_to users_path
+      else
+        redirect_to current_user
+      end
+    end
   end
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
+    if current_user.admin? && current_user.invitations.include?(@user) || current_user.eql?(@user)
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        redirect_to @user, notice: 'User was successfully updated.'
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render :edit
+      end
+    else # current_user is not @user and current_user did not invite @user
+      if current_user.admin?
+        redirect_to users_url, notice: "You can't destroy that user."
+      else
+        redirect_to current_user, notice: "You can't destroy that user."
       end
     end
   end
 
   # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
+    if current_user.admin? && current_user.invitations.include?(@user) || current_user.eql?(@user)
+      if @user.destroy
+        redirect_to users_url, notice: 'User was successfully destroyed.'
+      else
+        puts @user.errors.messages
+        render @user
+      end
+    else # current_user is not @user and current_user did not invite @user
+      if current_user.admin?
+        redirect_to users_url, notice: "You can't destroy that user."
+      else
+        redirect_to current_user, notice: "You can't destroy that user."
+      end
     end
   end
 
@@ -64,6 +81,8 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :first_name, :last_name)
+      keys = [:email, :first_name, :last_name]
+      keys << :tos_accepted if current_user.eql?(@user)
+      params.require(:user).permit(keys)
     end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,115 +13,45 @@ class UsersControllerTest < ActionController::TestCase
     @admin = users(:admin)
   end
 
-  test "should get index" do
-    # As a visitor
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-    assert assigns(:users).count == 0
-    assert_template :index
+  # UsersController#index
 
-    # As a resident
+  test "As a visitor, I will see no users when I visit the users index" do
+    get :index
+    assert_response :redirect
+    assert_nil assigns(:users)
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "As a resident, I can visit the users index" do
     sign_in @resident
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
     assert_not_nil assigns(:users).count > 0
     assert_template :index
-    sign_out @resident
+  end
 
-    # As an admin
+  test "As an admin, I can visit the users index" do
     sign_in @admin
     get :index
     assert_response :success
     assert_not_nil  assigns(:users).count > 0
     assert_template :index
-    sign_out @admin
   end
 
-  test "As a visitor, I cannot edit a user" do
-    get :edit, id: users(:resident).id
-    assert_response :redirect
-    assert_redirected_to new_user_session_path
-    assert_template :new
-  end
+  # UsersController#show
 
-
-  test "As a resident, I can edit my user record " do
-    sign_in @resident
-    get :edit, id: users(:resident).id
-    assert_response :success
-    assert_not_nil assigns(:user)
-    assert_template :edit
-    sign_out @resident
-  end
-
-  test "As a resident, I cannot edit other users" do
-    sign_in @resident
-    get :edit, id: users(:resident_1).id
-    assert_response :redirect
-    assert_redirected_to @user
-    assert_not_nil assigns(:user)
-    assert_template :index
-    sign_out @resident
-  end
-
-  test "As a visitor, I cannot update other users" do
-    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :redirect
-    assert_redirected_to new_user_session_path
-    assert_template :new
-  end
-
-  test "As a resident, I can update my user record" do
-    sign_in @resident
-    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :success
-    sign_out @resident
-  end
-
-  test "As a resident, I cannot update another user" do
-    sign_in @resident
-    # attempt to update a resident
-    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :redirect
-    assert_redirected_to new_user_session_path
-    assert_template :new
-
-    # attempt to update an admin
-    patch :update, id: @admins, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :redirect
-    assert_redirected_to new_user_session_path
-    assert_template :new
-    sign_out @resident
-  end
-
-  test "As an admin, I can udpate residents that were invited by me" do
-    sign_in @admin
-    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :success
-    sign_out @admin
-  end
-
-  test "As an admin, I cannot udpate residents that were not invited by my" do
-    sign_in @admin
-    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
-    assert_response :success
-    sign_out @admin
-  end
-
-  test "As a visitor, I cannot view a user" do
-    # attempt to view a resident
+  test "As a visitor, I cannot view a resident" do
     get :show, id: @resident
     assert_response :redirect
     assert_redirected_to new_user_session_path
-    assert_template :new
+  end
 
-    # attempt to view an admin
+  test "As a visitor, I cannot view an admin" do
     get :show, id: @admin
     assert_response :redirect
     assert_redirected_to new_user_session_path
-    assert_template :new
   end
 
   test "As a resident, I can view my user record" do
@@ -129,34 +59,34 @@ class UsersControllerTest < ActionController::TestCase
     get :show, id: @resident
     assert_response :success
     assert_template :show
-    sign_out @resident
   end
 
-  test "As a resident, I cannot view other users" do
-    # attempt to view another resident
+  test "As a resident, I cannot view another resident" do
     sign_in @resident
     get :show, id: users(:resident_1)
     assert_response :redirect
     assert_redirected_to @resident
-    assert_template :show
-    sign_out @resident
+  end
 
-    # attempt to view an admin
+  test "As a resident, I cannot view an admin" do
     sign_in @resident
     get :show, id: @admin
     assert_response :redirect
     assert_redirected_to @resident
+  end
+
+  test "As an admin, I can view my user record" do
+    sign_in @admin
+    get :show, id: @admin
+    assert_response :success
     assert_template :show
-    sign_out @resident
   end
 
   test "As an admin, I can view residents that were invited by me" do
     sign_in @admin
     get :show, id: users(:resident_1)
-    assert_response :redirect
-    assert_redirected_to @admin
+    assert_response :success
     assert_template :show
-    sign_out @admin
   end
 
   test "As an admin, I cannot view residents that were not invited by me" do
@@ -164,27 +94,139 @@ class UsersControllerTest < ActionController::TestCase
     get :show, id: @resident
     assert_response :redirect
     assert_redirected_to @admin
-    assert_template :show
-    sign_out @admin
   end
 
-  test "As a resident, I cannot destroy users" do
-    # attempt to destroy self
+  # UsersController#edit
+
+  test "As a visitor, I cannot edit a user" do
+    get :edit, id: users(:resident).id
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "As a resident, I can edit my user record " do
+    sign_in @resident
+    get :edit, id: users(:resident)
+    assert_response :success
+    assert_equal assigns(:user), @resident
+    assert_template :edit
+  end
+
+  test "As a resident, I cannot edit other user records" do
+    sign_in @resident
+    get :edit, id: users(:resident_1)
+    assert_equal assigns(:user), users(:resident_1)
+    assert_response :redirect
+    assert_redirected_to @resident
+  end
+
+  test "As an admin, I cannot edit user records not invited by me" do
+    sign_in @admin
+    get :edit, id: @resident
+    assert_equal assigns(:user), @resident
+    assert_response :redirect
+    assert_redirected_to users_path
+  end
+
+  test "As an admin, I can edit user records invited by me" do
+    sign_in @admin
+    get :edit, id: users(:resident_1)
+    assert_equal assigns(:user), users(:resident_1)
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "As a visitor, I cannot update other user records" do
+    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :redirect
+    assert_redirected_to new_user_session_path
+  end
+
+  test "As a resident, I cannot update my own user record, if I have not accepted the tos" do
+    sign_in @resident
+    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "As a resident, I can update my own user record, if I accept the tos" do
+    sign_in @resident
+    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated', tos_accepted: true }
+    assert_response :redirect
+    assert_redirected_to @resident
+  end
+
+  test "As a resident, I cannot update another resident" do
+    sign_in @resident
+    patch :update, id: users(:resident_1), user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :redirect
+    assert_redirected_to @resident
+  end
+
+  test "As a resident, I cannot update an admin" do
+    sign_in @resident
+    patch :update, id: @admin, user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :redirect
+    assert_redirected_to @resident
+  end
+
+  test "As an admin, when I update one of my invited residents that has not accepted the tos, the edit form renders" do
+    sign_in @admin
+    patch :update, id: users(:resident_1), user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :success
+    assert_template :edit
+  end
+
+  test "As an admin, when I update one of my invited residents that has accepted the tos, I am redirected" do
+    sign_in @admin
+    users(:resident_1).update!(tos_accepted: true)
+    patch :update, id: users(:resident_1), user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :redirect
+    assert_redirected_to users(:resident_1)
+  end
+
+  test "As an admin, I cannot update user records not invited by me" do
+    sign_in @admin
+    patch :update, id: @resident, user: { first_name: 'Updated', last_name: 'Updated' }
+    assert_response :redirect
+    assert_redirected_to users_path
+  end
+
+  test "As a visitor, I cannot destroy users" do
+    assert_difference('User.count', 0) do
+      delete :destroy, id: @admin
+    end
     assert_difference('User.count', 0) do
       delete :destroy, id: @resident
     end
     assert_response :redirect
-    assert_redirected_to users_path
+    assert_redirected_to new_user_session_path
+  end
 
-    # attempt to destroy another residents
-    assert_difference('User.count', 0) do
-      delete :destroy, id: users(:resident_1)
+  test "As a resident, I can destroy my own user record" do
+    sign_in @resident
+    assert_difference('User.count', -1) do
+      delete :destroy, id: @resident
     end
     assert_response :redirect
     assert_redirected_to users_path
+  end
 
-    # attempt to destroy an admin
+  test "As a resident, I cannot destroy other user records" do
+    sign_in @resident
     assert_difference('User.count', 0) do
+      delete :destroy, id: users(:resident_1)
+    end
+    assert_difference('User.count', 0) do
+      delete :destroy, id: @admin
+    end
+    assert_response :redirect
+    assert_redirected_to @resident
+  end
+
+  test "As an admin, I can destroy my own user record" do
+    sign_in @admin
+    assert_difference('User.count', -1) do
       delete :destroy, id: @admin
     end
     assert_response :redirect
@@ -192,15 +234,17 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "As an admin, I can destroy residents that were invited by me" do
+    sign_in @admin
     assert_difference('User.count', -1) do
       delete :destroy, id: users(:resident_1)
     end
-    assert_response :success
+    assert_response :redirect
     assert_redirected_to users_path
   end
 
   test "As an admin, I cannot destroy residents that were not invited by me" do
-    assert_difference('User.count', -1) do
+    sign_in @admin
+    assert_difference('User.count', 0) do
       delete :destroy, id: @resident
     end
     assert_response :redirect
