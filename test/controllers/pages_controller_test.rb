085f6233ab35c8509e34cebcbf0a33221c5f3c17
sealocal <local.mat@gmail.com>
2015-04-11 19:04:39 -0700

    add tests for pages controller

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class PagesControllerTest < ActionController::TestCase
+
+  # was the web request successful?
+  # was the user redirected to the right page?
+  # was the user successfully authenticated?
+  # was the correct object stored in the response template?
+  # was the appropriate message displayed to the user in the view?
+
+  setup do
+    @admin = users(:admin)
+    @resident = users(:resident)
+  end
+
+  test "As a visitor, I can visit the home page" do
+    get :index
+    assert_response :success
+  end
+
+  test "As a signed in resident, when I visit the home page, I am redirected" do
+    sign_in @resident
+    get :index
+    assert_response :redirect
+    assert_redirected_to new_encounter_path
+  end
+
+  test "As a signed in admin, when I visit the home page, I am redirected" do
+    sign_in @admin
+    get :index
+    assert_response :redirect
+    assert_redirected_to users_path
+  end
+
+end
