6e4bba756e2341f12e30e1fbe7ed5de258f2885a
sealocal <local.mat@gmail.com>
2015-04-11 17:14:20 -0700

    fix syntax error in user policy

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -14,7 +14,7 @@ class UserPolicy < ApplicationPolicy
     # current_user is an admin who invited the user in question
     ( user.admin? && record.invited_by_id == user.id ) ||
     # current_user is an admin within the same residency
-    ( user.admin? && user.residency == record.residency )
+    ( user.admin? && user.residency == record.residency ) ||
     # current_user is an admin_resident within the same residency
     ( user.admin_resident? && user.residency == record.residency )
   end
