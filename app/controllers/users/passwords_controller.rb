f2f7d3c565853403d87396eafb69219c9f3ec599
sealocal <local.mat@gmail.com>
2015-04-21 01:30:26 -0700

    fix PasswordsController#update token invalidation

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,9 +2,15 @@ class Users::PasswordsController < Devise::PasswordsController
 
   # PUT /users/password
   def update
-    super do |resource|
-      resource.tos_accepted = resource_params[:tos_accepted]
+    tos_accepted = resource_params[:tos_accepted]
+
+    self.resource = User.find_by(resource_params[:email])
+    if tos_accepted == "1"
+      resource.tos_accepted = tos_accepted
       resource.save
+      super
+    else
+      respond_with resource, location: edit_user_password_path(reset_password_token: resource_params[:reset_password_token])
     end
   end
 end
