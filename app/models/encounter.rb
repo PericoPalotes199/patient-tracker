ede27c4455ede83ac96febf568a9787c0b5acc96
sealocal <local.mat@gmail.com>
2018-04-06 01:21:03 -0700

    remove unused Encounter scopes

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,9 +1,5 @@
 class Encounter < ActiveRecord::Base
 
-  scope :adult_medicine, ->{ where encounter_type: 'adult_medicine' }
-  scope :icu, ->{ where encounter_type: 'icu' }
-  scope :encountered_today, ->{ where 'created_at > ?', Time.zone.today }
-
   belongs_to :user
 
   validates :encounter_type, presence: true
