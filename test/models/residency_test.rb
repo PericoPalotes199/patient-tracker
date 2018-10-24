321c4ee762c08e9230b4a24d14248da73c4d6e2a
sealocal <local.mat@gmail.com>
2018-10-23 20:50:47 -0700

    add name validation for residency

--- test/models/residency_test.rb
+++ test/models/residency_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ResidencyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "a valid residency" do
+    assert_same residencies(:valid_residency).valid?, true
+  end
+
+  test "name is required" do
+    residency = Residency.new(name: nil)
+    assert_same(residency.valid?, false)
+    assert_equal(residency.errors.messages.first, [:name, ["can't be blank"]])
+  end
 end
