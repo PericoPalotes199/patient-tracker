0950d28eca21f7ed78cea605cb84ba8537e7b4a6
sealocal <local.mat@gmail.com>
2019-01-15 00:30:18 -0800

    update Users::PasswordsController#update for devise 3.5.x

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -4,20 +4,22 @@ class Users::PasswordsController < Devise::PasswordsController
   def update
     tos_accepted = resource_params[:tos_accepted]
 
-    # found in devise-invitable's #reset_password_by_token method
-    original_token       = resource_params[:reset_password_token]
-    reset_password_token = Devise.token_generator.digest(self, :reset_password_token, original_token)
-    # find *without* initializing with an error
-    self.resource = User.find_by(reset_password_token: reset_password_token)
+    if tos_accepted != "1"
+      flash[:alert] = 'Terms of service must be accepted.'
+      respond_with(resource, location: edit_user_password_path(resource_params.slice(:reset_password_token))) and return
+    end
 
-    if tos_accepted == "1"
+    if resource_params[:password] != resource_params[:password_confirmation]
+      flash[:alert] = 'Passwords must match.'
+      respond_with(resource, location: edit_user_password_path(resource_params.slice(:reset_password_token))) and return
+    end
+
+    # Continue with Devise default functionality
+    # https://github.com/plataformatec/devise/blob/v3.5.10/app/controllers/devise/passwords_controller.rb#L35
+    super do
       resource.tos_accepted = tos_accepted
       resource.save
-      super
-    else
-      flash[:alert] = 'Terms of service must be accepted.'
-      respond_with resource,
-        location: edit_user_password_path(reset_password_token: resource_params[:reset_password_token])
     end
+
   end
 end
